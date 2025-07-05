9b42bf0161d3952f7fc34543756ac125f26edb5c
David Brown <dfbrownnz@gmail.com>
2025-07-05 21:00:48 +0000

    ifk

--- basic.js
+++ basic.js
@@ -27,10 +27,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   const files = listFilesAndFolders(directoryPathDownload);
     //fileAddRcd, fileReadRcd
-  fileAddRcd(rcd);
+  //fileAddRcd(rcd);
   //res.send(`Files and Folders in <PRE>{${JSON.stringify(files, null, 2) }} </PRE>`);
   let filesSorted = files.sort((a, b) => a.name.localeCompare(b.name));
-  filesSorted = filesSorted.sort((a, b) => a.type.localeCompare(b.type));
+  // filesSorted = filesSorted.sort((a, b) => a.type.localeCompare(b.type));
   res.send(`<h1>Files and Folders in ${directoryPathDownload}</h1><ul>${filesSorted.map(file => `<li>${file.type} ${file.name} </li>`).join('')}</ul>`);
   console.log(`Request received for ${req.url}`);
 })
@@ -65,22 +65,39 @@ app.get('/rcd/:fileName', (req, res) => {
 
 
 // http://localhost:3000/rcd
+// https://8080-cs-19c0e239-0598-41ad-9958-776d502c7ba3.cs-us-east1-vpcf.cloudshell.dev/rcd?authuser=0
 app.get('/rcd', (req, res) => {
-   // console.log(`Request received for ${req.url} with : ${req.params}`);
+   console.log(` get /rcd Request received for ${req.url} `);
   const rcds = fileReadRcd();
   //return res.json(rcds);
   return res.send(`<div>rcds  in ${ JSON.stringify(rcds, null, 2) }</div>`);
 })
 
+// https://8080-cs-19c0e239-0598-41ad-9958-776d502c7ba3.cs-us-east1-vpcf.cloudshell.dev/rcd?authuser=0
 // curl -X POST http://localhost:3000/rcd -H "Content-Type: application/json" -d "{\"name\":\"from curl\",\"action\":\"idk\"}"
 app.post('/rcd', (req, res) => {
-  console.log(`Request received for ${req.url} with body: ${JSON.stringify(req.body)}`);
+  console.log(`POST Request received for ${req.url} with body: ${JSON.stringify(req.body)}`);
   fileAddRcd(req.body);
   // For example, you could save it to a database or perform some action
   res.json({ message: 'Record added successfully', data: req.body });
 })
 
-app.listen(port, () => {
-  console.log(`C:\Users\dfbro\Desktop\code\nodejs\basic\basic.js app listening on port http://localhost:${port}/`)
-})
+// app.listen(port, () => {
+//   //console.log(`C:\Users\dfbro\Desktop\code\nodejs\basic\basic.js app listening on port http://localhost:${port}/`)
+//   console.log(` app listening on port ${port}`)
+// })
 
+const server = app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+  console.log(`Access it at http://localhost:${port}/`);
+});
+
+server.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Error: Port ${port} is already in use. Please close the other process or choose a different port.`);
+    //  sudo netstat -tulnp | grep :8080
+    // sudo kill 4629
+  } else {
+    console.error(`Server startup error: ${err}`);
+  }
+});
--- utils/opFileReadWrite.js
+++ utils/opFileReadWrite.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const filePathData = path.join(__dirname, 'data.json');
+const filePathData = path.join(__dirname, '../data/data.json');
 // export interface Rcd {
 //   name: string;
 //   action: string;
